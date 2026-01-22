cf1eef89c68dcf301370cbff8a219de004453965
savindaJ <thantrige32@gmail.com>
2026-01-22 10:59:57 +0530

    Initial Commit

---
+++ roomac/roomacApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomacApp.swift
+//  roomac
+//
+//  Created by savinda jayasekara on 2026-01-22.
+//
+
+import SwiftUI
+
+@main
+struct roomacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ roomacTests/roomacTests.swift
@@ -0,0 +1,17 @@
+//
+//  roomacTests.swift
+//  roomacTests
+//
+//  Created by savinda jayasekara on 2026-01-22.
+//
+
+import Testing
+@testable import roomac
+
+struct roomacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
